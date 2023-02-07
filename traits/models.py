b5f479b251214b834daaddf1c9527dd891a8cc59
NattanSilva <natanxboy@gmail.com>
2023-02-07 17:43:09 -0300

    Add: trait created_at

--- traits/models.py
+++ traits/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Trait(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __repr__(self) -> str:
         return f"<Trait ({self.id}) - {self.name}>"
